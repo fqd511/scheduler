7661723eb0e6bd4d49322b7c825eeb5f83bf8ed0
511 <fqd511@163.com>
2022-07-18 13:54:51 +0800

    fix: execute sequence

--- utils.ts
+++ utils.ts
@@ -3,90 +3,89 @@
  * @Desc: utils
  */
 
- import { Log, LogLevelEnum, LogSourceTypeEnum } from "./type";
- import { Client } from "@notionhq/client";
- 
- /**
-  * taking advantage of the fact that Sweden uses a format very close to ISO
-  * see: https://stackoverflow.com/a/65758103/6355803
-  * @param date
-  * @returns
-  */
- export function getISODate(date = new Date()) {
-   return new Date()
-     .toLocaleString("sv", {
-       year: "numeric",
-       month: "numeric",
-       day: "numeric",
-       hour: "numeric",
-       minute: "numeric",
-       second: "numeric",
-       // @ts-ignore
-       fractionalSecondDigits: 3,
-     })
-     .replace(",", ".")
-     .replace(" ", "T");
- }
- 
- /**
-  * formet log into notion-format and write to local file
-  * @param log
-  */
- export function log(log: Partial<Log>) {
-   const {
-     type = LogSourceTypeEnum.Other,
-     name = "",
-     desc = "",
-     timestamp = getISODate(),
-     level = LogLevelEnum.info,
-   } = log;
-   const logItem = {
-     name: {
-       title: [
-         {
-           text: {
-             content: name,
-           },
-         },
-       ],
-     },
-     level: {
-       select: {
-         name: level,
-       },
-     },
-     type: {
-       select: {
-         name: type,
-       },
-     },
-     desc: {
-       rich_text: [
-         {
-           text: {
-             content: desc,
-           },
-         },
-       ],
-     },
-     timestamp: {
-       date: {
-         start: timestamp,
-       },
-     },
-   };
- 
-   // Initializing a client
-   const notion = new Client({
-     auth: process.env.NOTION_TOKEN,
-   });
- 
-   notion.pages.create({
-     parent: {
-       type: "database_id",
-       database_id: process.env.DATABASE_ID!,
-     },
-     properties: logItem,
-   });
- }
- 
+import { Log, LogLevelEnum, LogSourceTypeEnum } from "./type";
+import { Client } from "@notionhq/client";
+
+/**
+ * taking advantage of the fact that Sweden uses a format very close to ISO
+ * see: https://stackoverflow.com/a/65758103/6355803
+ * @param date
+ * @returns
+ */
+export function getISODate(date = new Date()) {
+  return new Date()
+    .toLocaleString("sv", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      // @ts-ignore
+      fractionalSecondDigits: 3,
+    })
+    .replace(",", ".")
+    .replace(" ", "T");
+}
+
+/**
+ * formet log into notion-format and write to local file
+ * @param log
+ */
+export function log(log: Partial<Log>): Promise<any> {
+  const {
+    type = LogSourceTypeEnum.Other,
+    name = "",
+    desc = "",
+    timestamp = getISODate(),
+    level = LogLevelEnum.info,
+  } = log;
+  const logItem = {
+    name: {
+      title: [
+        {
+          text: {
+            content: name,
+          },
+        },
+      ],
+    },
+    level: {
+      select: {
+        name: level,
+      },
+    },
+    type: {
+      select: {
+        name: type,
+      },
+    },
+    desc: {
+      rich_text: [
+        {
+          text: {
+            content: desc,
+          },
+        },
+      ],
+    },
+    timestamp: {
+      date: {
+        start: timestamp,
+      },
+    },
+  };
+
+  // Initializing a client
+  const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+
+  return notion.pages.create({
+    parent: {
+      type: "database_id",
+      database_id: process.env.DATABASE_ID!,
+    },
+    properties: logItem,
+  });
+}
