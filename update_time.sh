3921e208150eb8e53f45bae7a75533679027db0f
fanqidi <fqd@xkool.org>
2023-02-06 15:09:17 +0800

    feat(CI): add update.yaml

--- update_time.sh
+++ update_time.sh
@@ -1,4 +1,6 @@
 echo $(date) > updated_at
+git config user.email "15816056+fqd511@users.noreply.github.com "
+git config user.name "bot"
 git add .
 git commit -m "chore: Update log by Github Action"
 git push origin main
