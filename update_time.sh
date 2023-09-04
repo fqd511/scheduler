96f14895331626c636bb3e5940811e3e92667e41
v <15816056+fqd511@users.noreply.github.com>
2023-09-04 12:32:57 +0800

    add throttling to auto-log

--- update_time.sh
+++ update_time.sh
@@ -1,6 +1,17 @@
-echo $(date) > updated_at
-git config user.email "15816056+fqd511@users.noreply.github.com "
-git config user.name "bot"
-git add .
-git commit -m "chore: Update log by Github Action"
-git push origin main
+# get current date
+current_date=$(date +%d)
+
+# filter date
+if (( current_date % 9 == 0 )); then
+    echo "commit log"
+    # update date file and commit
+    echo $(date) > updated_at
+    git config user.email "15816056+fqd511@users.noreply.github.com "
+    git config user.name "bot"
+    git add .
+    git commit -m "chore: Update log by Github Action"
+    git push origin main
+else
+    echo "skip log"
+    exit 1
+fi
