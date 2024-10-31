840b051c50b7150b0264f9ae2839c28c2d0a8aa7
v <15816056+fqd511@users.noreply.github.com>
2024-10-31 11:30:19 +0800

    fix: add error catch

--- jobs/gfw.ts
+++ jobs/gfw.ts
@@ -22,27 +22,39 @@ export const GFWCheckIn = async (username: string, pwd: string) => {
 
   // incase there is a notification popup
   const tipButton = await page.locator("text=Read");
+
+  if (page.isClosed()) {
+    throw new Error("The page is closed before clicking the tip button");
+  }
+
   if (tipButton) {
-    tipButton.click({ timeout: 0 });
+    await tipButton.waitFor({ state: "visible", timeout: 20000 }); // 等待可见
+    await tipButton.click({ timeout: 20000 });
   }
 
   const checkInLabel = await page.locator("#checkin-div").textContent();
 
-  if (checkInLabel?.trim() === "每日签到") {
-    await page.locator("#checkin-div").click();
-    const resultTitleLabel = await page.locator("#swal2-title").textContent();
-    const resultContentLabel = await page
-      .locator("#swal2-content")
-      .textContent();
-    if (resultTitleLabel?.trim() === "签到成功") {
-      await browser.close();
-      return `签到成功,${resultContentLabel}`;
+  try {
+    if (checkInLabel?.trim() === "每日签到") {
+      await page.locator("#checkin-div").click();
+      const resultTitleLabel = await page.locator("#swal2-title").textContent();
+      const resultContentLabel = await page
+        .locator("#swal2-content")
+        .textContent();
+      if (resultTitleLabel?.trim() === "签到成功") {
+        await browser.close();
+        return `签到成功,${resultContentLabel}`;
+      } else {
+        throw new Error(
+          `gfw fail:签完之后 不是 签到成功,而是${resultTitleLabel?.trim()}`
+        );
+      }
     } else {
-      throw new Error(
-        `gfw fail:签完之后 不是 签到成功,而是${resultTitleLabel?.trim()}`
-      );
+      throw new Error("gfw fail:不是 每日签到,而是" + checkInLabel?.trim());
     }
-  } else {
-    throw new Error("gfw fail:不是 每日签到,而是" + checkInLabel);
+  } catch (error) {
+    console.error("An error occurred:", error);
+    await browser.close(); // 确保浏览器关闭
+    throw error; // 重新抛出错误
   }
 };
